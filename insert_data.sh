ab14af70fc15409a5b9bb76631f04e9e9b119fe0
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 23:23:48 +0000

    Save progress: 1049.1

--- insert_data.sh
+++ insert_data.sh
@@ -49,11 +49,11 @@ do
   if [[ $FIRST != "first_name" ]]; then
     # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-    echo $MAJOR_ID
     # if not found
     if [[ -z $MAJOR_ID ]]; then
       # set to null
       MAJOR_ID=null
+    echo $MAJOR_ID
     fi
     # insert student
   fi 

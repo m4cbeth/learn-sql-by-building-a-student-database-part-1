c57d8789ccfda94ce815a61d54cbccfeed9550be
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 22:34:07 +0000

    Save progress: 750.1

--- insert_data.sh
+++ insert_data.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+
 # Script to insert data from courses.csv and students.csv into students database
+
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
-# $($PSQL "query here")
+
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
+  if [[ $MAJOR != major ]]; then
 
-# get major_id
-MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major = '$MAJOR';" )
-# if not found
-if [[ -z $MAJOR_ID ]]; then
-# insert major
-INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR');")
-echo $INSERT_MAJOR_RESULT
-# get new major_id
-fi
-# get course_id
-# if not found
-# insert course
-# get new course_id
-# insert into majors_courses
+  # get major_id
+  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
+    # if not found
+    if [[ -z $MAJOR_ID ]]
+    then
+      # insert major
+      INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+      echo $INSERT_MAJOR_RESULT
 
+      # get new major_id
+
+    fi
+  # get course_id
+
+  # if not found
+
+  # insert course
+
+  # get new course_id
+
+  # insert into majors_courses
+
+  fi
 done
